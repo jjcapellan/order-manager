d467413f5db19e8400b2e325c1bd82a0aac08264
jjcapellan <soycape@hotmail.com>
2022-11-26 01:57:18 +0100

    PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+/**
+ * Info:
+ * https://parceljs.org/languages/javascript/#service-workers
+ */
+
+import { manifest, version } from '@parcel/service-worker';
+
+async function install() {
+  const cache = await caches.open(version);
+  await cache.addAll(manifest);
+}
+self.addEventListener('install', e => e.waitUntil(install()));
+
+async function activate() {
+  const keys = await caches.keys();
+  await Promise.all(
+    keys.map(key => key !== version && caches.delete(key))
+  );
+}
+self.addEventListener('activate', e => e.waitUntil(activate()));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWidth(
+    matches(e.request)
+      .then((res) => {
+        return res || fetch(e.request);
+      })
+      .catch(console.log)
+  );
+});
